5f5cd08d9d87f9da61fd6957ba6a1a298712650d
Seth Staggs <sethstaggs2@gmail.com>
2021-01-04 18:20:07 -0600

    created dependencies

---
+++ emp.js
@@ -0,0 +1,3 @@
+var mysql = require("mysql"); // connects to mysql
+var inquirer =  require("inquirer"); // interacts with user through cmd line
+var promisemysql = require("promise-mysql"); // Asyncronous 
